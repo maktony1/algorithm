e495cfba1e7970ef7c9000e7594e7cb9863a438b
maktony1 <ghkdehdgnl12@naver.com>
2021-04-29 17:45:26 +0900

    수정

--- src/step1/Main_1001.java
+++ src/step1/Main_1001.java
@@ -1,8 +1,8 @@
-package baekjoon_1001;
+package step1;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main_1001 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
