83a92574e48a013802242b4cdd9a04133acc6182
maktony1 <ghkdehdgnl12@naver.com>
2021-05-02 21:19:54 +0900

    test

--- src/baekjoon/step3/Main_15552.java
+++ src/baekjoon/step3/Main_15552.java
@@ -1,27 +1,25 @@
 package baekjoon.step3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main_15552 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        String str = br.readLine();
         int tc = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < tc; i++) {
+            st = new StringTokenizer(br.readLine());
 
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum=a+b;
+            bw.write(sum);
         }
-        StringTokenizer st = new StringTokenizer(str);
+        bw.flush();
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-
-        System.out.println("a="+a);
-        System.out.println("b="+b);
-//        String array[] = str.split(" ");
     }
 }
