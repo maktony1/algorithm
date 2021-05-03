ee5a8d7e1e6de6bc648a132bfb85dba573b82304
maktony1 <ghkdehdgnl12@naver.com>
2021-05-03 15:43:45 +0900

    BurfferedReader & BufferedWriter

--- src/baekjoon/step3/Main_2741.java
+++ src/baekjoon/step3/Main_2741.java
@@ -1,7 +1,21 @@
 package baekjoon.step3;
 
-public class Main_2741 {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Main_2741 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= num; i++) {
+            bw.write(String.valueOf(i));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
