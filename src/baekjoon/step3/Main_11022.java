f8d7af896a6823dce7f48eee0c1eced0424d519e
maktony1 <ghkdehdgnl12@naver.com>
2021-05-03 17:05:27 +0900

    BurfferedReader & BufferedWriter

--- src/baekjoon/step3/Main_11022.java
+++ src/baekjoon/step3/Main_11022.java
@@ -1,7 +1,28 @@
 package baekjoon.step3;
 
-public class Main_11022 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class Main_11022 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
+        StringTokenizer st;
+
+        int tc = Integer.parseInt(br.readLine());
+        int a,b;
+
+        for (int i = 1; i <= tc; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #"+i+": "+a+" + "+b+" = "+ (a+b)+"\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
