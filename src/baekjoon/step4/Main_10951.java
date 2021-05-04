ea39a97c5fc92071f68e8270f2d6c5971f76d6d7
maktony1 <ghkdehdgnl12@naver.com>
2021-05-04 16:53:11 +0900

    tokenizer...

--- src/baekjoon/step4/Main_10951.java
+++ src/baekjoon/step4/Main_10951.java
@@ -1,7 +1,31 @@
 package baekjoon.step4;
 
-public class Main_10951 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class Main_10951 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int a, b;
+        String str = "";
+
+        while (  (str= br.readLine()) != null   ) {
+
+            st = new StringTokenizer(str, " ");
+
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            bw.write(String.valueOf(a + b));
+            bw.newLine();
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
     }
 }
+//여기서는 이 코드가 안돌아감...몇시간 헤맸다 하...
--- src/baekjoon/step4/Main_1110.java
+++ src/baekjoon/step4/Main_1110.java
@@ -1,7 +1,28 @@
 package baekjoon.step4;
 
-public class Main_1110 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class Main_1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        String initValue = br.readLine();
+        String a,b;
+        int sum;
+        System.out.println("initValue="+initValue);
+//        while (true){
+        st = new StringTokenizer(initValue, "");
+
+        a = st.nextToken();
+        System.out.println("a="+a);
+        b = st.nextToken();
+        System.out.println("b="+b);
+
+        sum = Integer.parseInt(a + b);
+        System.out.println("sum=" + sum);
+//        }
     }
 }
