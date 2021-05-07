8692c73ffec6a5414f1d56e4713138eae4e09e82
maktony1 <ghkdehdgnl12@naver.com>
2021-05-07 17:49:50 +0900

    ....생각보다 어렵게품

--- src/baekjoon/step4/Main_1110.java
+++ src/baekjoon/step4/Main_1110.java
@@ -1,28 +1,35 @@
 package baekjoon.step4;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main_1110 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
-        String initValue = br.readLine();
-        String a,b;
-        int sum;
-        System.out.println("initValue="+initValue);
-//        while (true){
-        st = new StringTokenizer(initValue, "");
+        int initValue = Integer.parseInt(br.readLine());
+        br.close();
 
-        a = st.nextToken();
-        System.out.println("a="+a);
-        b = st.nextToken();
-        System.out.println("b="+b);
+        int num = 0;
+        int sum = initValue;
+        int count = 0;
+        if (initValue == 0) {
+            bw.write("0");
+        } else {
 
-        sum = Integer.parseInt(a + b);
-        System.out.println("sum=" + sum);
-//        }
+            while (initValue != num) {
+                count++;
+                sum = (sum % 10 * 10) + (sum / 10 + sum % 10) % 10;
+
+                num = sum;
+                if (initValue == num) {
+                    break;
+                }
+            }
+
+            bw.write(String.valueOf(count));
+        }
+            bw.flush();
+            bw.close();
     }
 }
