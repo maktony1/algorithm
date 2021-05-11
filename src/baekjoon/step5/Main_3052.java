7568da8d7980c4cd7bfb66d5ebf67e42adb88003
maktony1 <ghkdehdgnl12@naver.com>
2021-05-11 17:51:01 +0900

    3052

--- src/baekjoon/step5/Main_3052.java
+++ src/baekjoon/step5/Main_3052.java
@@ -1,10 +1,22 @@
 package baekjoon.step5;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Main_3052 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int[] arr = new int[10];
+        int cnt = 10;
 
+        for (int i = 0; i < 10; i++) {
+            arr[i] = (Integer.parseInt(br.readLine()) % 42);
+            System.out.println(arr[i]);
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+        }
     }
 }
