3ee0a00f8bf4df596dbc6caf9fb96db04c0f4d84
maktony1 <ghkdehdgnl12@naver.com>
2021-05-12 14:23:14 +0900

    Hashset 사용 방법 Iterator 사용방법도

--- src/baekjoon/step5/Main_3052.java
+++ src/baekjoon/step5/Main_3052.java
@@ -1,22 +1,59 @@
 package baekjoon.step5;
 
 import java.io.*;
+import java.util.HashSet;
 
 public class Main_3052 {
+    /**
+     * HashSet을 사용하여 중복 제거 하는 방법
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] arr = new int[10];
-        int cnt = 10;
-
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < 10; i++) {
-            arr[i] = (Integer.parseInt(br.readLine()) % 42);
-            System.out.println(arr[i]);
+            set.add(Integer.parseInt(br.readLine()) % 42);
         }
-
-        for (int i = 0; i < 10; i++) {
-
-        }
+        br.close();
+        bw.write(String.valueOf(set.size()));
+        bw.flush();
+        bw.close();
     }
+    //        System.out.println(set.size());
+    //        System.out.println(set);
+    //        Iterator iter = set.iterator();
+    //
+    //        for (int i = 0; i < 10; i++) {
+    //            iter.hasNext();
+    //            System.out.println(iter.next());
+    //        }
+    //    }
+    /**
+     * 그냥 일반 배열을 사용해서 중복 체크
+     * */
+    //    public static void main(String[] args) throws IOException {
+    //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    //
+    //        boolean[] arr = new boolean[42];
+    //
+    //        for (int i = 0; i < 10; i++) {
+    //            arr[(Integer.parseInt(br.readLine()) % 42)] = true;
+    //        }
+    //        br.close();
+    //
+    //
+    //        int cnt = 0;
+    //        for (int i = 0; i < arr.length; i++) {
+    //            if (arr[i] == true) {
+    //                cnt++;
+    //            }
+    //        }
+    //        bw.write(String.valueOf(cnt));
+    //        bw.flush();
+    //        bw.close();
+    //    }
+
 }
+
