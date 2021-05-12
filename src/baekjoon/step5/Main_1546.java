f60fddff60fdac089caee8ad8547d35681e217c8
maktony1 <ghkdehdgnl12@naver.com>
2021-05-12 16:43:32 +0900

    arr에 split으로 배열 생성

--- src/baekjoon/step5/Main_1546.java
+++ src/baekjoon/step5/Main_1546.java
@@ -1,10 +1,40 @@
 package baekjoon.step5;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main_1546 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int tc = Integer.parseInt(st.nextToken());
+        double[] arr = new double[tc];
+
+        //다음줄 받아오기
+        st = new StringTokenizer(br.readLine());
+        //받아서 배열에 삽입
+        for (int i = 0; i < tc; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        double max = 0;
+        //비교 하기
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = (sum + (arr[i] / max * 100));
+        }
+        bw.write(String.valueOf(sum/arr.length));
+        bw.flush();
+        bw.close();
 
     }
 }
--- src/baekjoon/step5/Main_8958.java
+++ src/baekjoon/step5/Main_8958.java
@@ -1,10 +1,40 @@
 package baekjoon.step5;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main_8958 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int tc = Integer.parseInt(br.readLine());
+        String[] arr;
+        int sum;
+        int score;
 
+        //테스트케이스 반복
+        for (int i = 0; i < tc; i++) {
+            //OX 받아와서 배열 생성
+            arr = br.readLine().split("");
+            sum = 0;
+            score = 1;
+            //배열 OX 체크 부분
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j].equals("O")) {
+                    sum += score++;
+                }
+                if (arr[j].equals("X")) {
+                    score = 1;
+                }
+            }
+            bw.write(String.valueOf(sum));
+            bw.newLine();
+        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
+
+//
