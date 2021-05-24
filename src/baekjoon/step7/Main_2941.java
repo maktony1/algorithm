e47437471ff2cbfe7d861fe081144bfff81167a8
maktony1 <ghkdehdgnl12@naver.com>
2021-05-24 17:37:29 +0900

    크로아티아 알파벳 체크

--- src/baekjoon/step7/Main_2941.java
+++ src/baekjoon/step7/Main_2941.java
@@ -1,9 +1,14 @@
 package baekjoon.step7;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main_2941 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String CroatiaAlphabet =br.readLine();
 
     }
 }
