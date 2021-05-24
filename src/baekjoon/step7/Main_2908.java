2a278466906d17e989e078bafa596bca12ab3a1f
maktony1 <ghkdehdgnl12@naver.com>
2021-05-24 16:26:59 +0900

    문자열 연산 문자 자리바꾸기

--- src/baekjoon/step7/Main_2908.java
+++ src/baekjoon/step7/Main_2908.java
@@ -1,10 +1,23 @@
 package baekjoon.step7;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main_2908 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+        String firstNum = st.nextToken();
+        String secondNum = st.nextToken();
+        firstNum = String.valueOf(firstNum.charAt(2)) + firstNum.charAt(1) + firstNum.charAt(0);
+        secondNum = String.valueOf(secondNum.charAt(2)) + secondNum.charAt(1) + secondNum.charAt(0);
+
+        if (Integer.parseInt(firstNum) > Integer.parseInt(secondNum)) {
+            System.out.println(firstNum);
+        } else {
+            System.out.println(secondNum);
+        }
     }
 }
 
