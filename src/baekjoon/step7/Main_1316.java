d6aeac76ef3b670f06db1f5a46c5392912bb3a87
maktony1 <ghkdehdgnl12@naver.com>
2021-06-04 15:48:41 +0900

    그룹단어체커 1316번

--- src/baekjoon/step7/Main_1316.java
+++ src/baekjoon/step7/Main_1316.java
@@ -15,29 +15,15 @@ public class Main_1316 {
         for (int i = 0; i < tc; i++) {
             System.out.println();
             str = br.readLine();
-
-            //첫 단어 체크
-            loop:
-            for (int j = 0; j < str.length(); j++) {
-
-                //3글자 미만 일 경우
-                if (3 > str.length()) {
-                    cnt++;
-                    break loop;
-                }//3글자 미만 일 경우 끝
-
-                //3글자 이상 일 경우
-                if (3 <= str.length()) {
-                    if (j < str.length() - 2) {
-                        for (int k = j; k < str.length() - 2; k++) {
-                            if (str.charAt(k) != str.charAt(k + 1) && str.charAt(k) == str.charAt(k + 2)) {
-                                break loop;
-                            }
-                        }
-                    }
-                }
+            //3글자 미만은 무조건 그룹단어로 체크
+            if (str.length() < 3) {
+                cnt++;
             }
-            cnt++;
+
+            //3글자 이상부터 체크하기
+
+
+
         }//end of tc
         System.out.println("cnt = " + cnt);
     }
