994b493d41715181900d7b6239c94e59bdd938ac
maktony1 <ghkdehdgnl12@naver.com>
2021-06-09 20:42:56 +0900

    그룹단어체커 1316번

--- src/baekjoon/step7/Main_1316.java
+++ src/baekjoon/step7/Main_1316.java
@@ -11,19 +11,23 @@ public class Main_1316 {
         int tc = Integer.parseInt(br.readLine());
         String str;
         int cnt = 0;
+        String[] charArray;
 
         for (int i = 0; i < tc; i++) {
-            System.out.println();
             str = br.readLine();
             //3글자 미만은 무조건 그룹단어로 체크
             if (str.length() < 3) {
                 cnt++;
             }
+            if (str.length() > 2) {
+                charArray = new String[str.length()];
+                charArray =str.split("");
+                System.out.println(charArray.length);
+                for (int j = 0; j < str.length(); j++) {
+                }
+            }
 
             //3글자 이상부터 체크하기
-
-
-
         }//end of tc
         System.out.println("cnt = " + cnt);
     }
